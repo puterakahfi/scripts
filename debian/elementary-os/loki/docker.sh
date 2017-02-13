55cd50b23346e42cbf7fab154c26c51594fc52a4
puterakahfi <puterakahfi@gmail.com>
2017-02-13 18:37:27 +0700

    updated docker install script

--- debian/elementary-os/loki/docker.sh
+++ debian/elementary-os/loki/docker.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-sudo apt-get install apt-transport-https ca-certificates -y
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo rm -f /etc/apt/sources.list.d/docker.list
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 sudo apt-get update
-sudo apt-get purge lxc-docker
-
-sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
-sudo apt-get install docker-engine
-sudo service docker start
-
+apt-cache policy docker-engine
+sudo apt-get install -y docker-engine
 sudo groupadd docker
 sudo usermod -aG docker $USER
+sudo service docker start
+sudo docker info 
