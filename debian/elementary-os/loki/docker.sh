0146d09deae6f146a47e068beed6afdfecb6c494
puterakahfi <puterakahfi@gmail.com>
2017-02-13 21:47:52 +0700

    add lemp.sh

--- debian/elementary-os/loki/docker.sh
+++ debian/elementary-os/loki/docker.sh
@@ -7,4 +7,9 @@ sudo apt-get install -y docker-engine
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo service docker start
+
+#install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.11.1/docker-compose-`uname -s`-`uname -m` > sudo /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 sudo docker info 
---
+++ debian/lemp.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+
+# nginx
+sudo apt-get install nginx -y
+
+sudo ufw allow 'Nginx HTTP'
+
+#mysql
+sudo apt-get install mysql-server -y
+
+#sudo mysql_secure_installation
+sudo apt-get install php-fpm php-mysql -y
+sudo systemctl restart php7.0-fpm
+
+
