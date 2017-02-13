8ed30f2b952eb7fe4ce130001231f8f2c2ea363e
puterakahfi <puterakahfi@gmail.com>
2017-02-13 18:01:43 +0700

    test update after use ssh

--- debian/node.sh
+++ debian/node.sh
@@ -14,7 +14,7 @@ do
             ;;
     esac
 
-done < results
+done
 sudo apt-get install python-software-properties -y
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install nodejs -y
