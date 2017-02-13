d83c70635a009c5119d1e06dfd640e63257aa5ae
puterakahfi <puterakahfi@gmail.com>
2017-02-13 12:04:02 +0700

    add elementary

---
+++ elementary-os/terminal.sh
@@ -0,0 +1,8 @@
+#/usr/bin
+
+# install oh my zsh 
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install tmux
+sudo apt install tmux -y
