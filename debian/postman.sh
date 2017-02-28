4372c97d50ce16bc1cfaaa80cd12a732a597dafd
puterakahfi <puterakahfi@gmail.com>
2017-02-28 09:58:18 +0700

    add script to install postman

---
+++ debian/postman.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+
+#wget    --output-document=postman.tar.gz https://dl.pstmn.io/download/latest/linux64
+#tar -xvzf postman.tar.gz
+sudo mv -f  Postman/ /usr/share
+sudo ln -sf /usr/share/Postman/Postman /usr/local/bin/
+rm postman.tar.gz
+
+
