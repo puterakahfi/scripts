edeb31bc23336dc7fcdaabbe5088d3d9e032d936
puterakahfi <puterakahfi@gmail.com>
2017-03-01 18:46:19 +0700

    update script postman

--- debian/postman.sh
+++ debian/postman.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 
-#wget    --output-document=postman.tar.gz https://dl.pstmn.io/download/latest/linux64
-#tar -xvzf postman.tar.gz
+wget    --output-document=postman.tar.gz https://dl.pstmn.io/download/latest/linux64
+tar -xvzf postman.tar.gz
 sudo mv -f  Postman/ /usr/share
 sudo ln -sf /usr/share/Postman/Postman /usr/local/bin/
 rm postman.tar.gz
