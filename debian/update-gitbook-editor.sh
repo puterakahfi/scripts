7539f1c5eb18a7a0748d818eb5ba3923c7241999
puterakahfi <puterakahfi@gmail.com>
2017-03-01 08:28:49 +0700

    add script for update gitbook editor

---
+++ debian/update-gitbook-editor.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+
+wget --output-document gitbook-editor.deb http://downloads.editor.gitbook.com/download/linux-64-bit
+if ! type "gdebi" > /dev/null; then
+    sudo apt-get install -y  gdebi
+    # install foobar here
+fi
+sudo gdebi  gitbook-editor.deb
+rm gitbook-editor.deb
+echo " \n gitbook editor has been updatedd "
