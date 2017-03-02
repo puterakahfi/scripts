424a0c95e9d48a02d1a7a5231348c8dadd4766a5
puterakahfi <puterakahfi@gmail.com>
2017-03-02 10:01:04 +0700

    add themes directory

---
+++ debian/themes/moka.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+sudo add-apt-repository ppa:moka/daily
+sudo apt update
+sudo apt install -y moka-icon-theme faba-icon-theme faba-mono-icons
+
+
---
+++ debian/themes/osx-arc-black.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+
+wget --output-document=osx-arc.zip https://github.com/horst3180/arc-theme/archive/master.zip
+sudo unzip osx-arc.zip -d ~/.themes/
+
