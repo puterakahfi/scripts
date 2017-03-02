b414405e34028fd2964d5f96b99f872235a8dd9a
puterakahfi <puterakahfi@gmail.com>
2017-03-02 10:37:44 +0700

    add arc theme

---
+++ debian/themes/arc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
+sudo apt-get update
+sudo apt-get install -y --allow-unauthenticated  arc-theme
