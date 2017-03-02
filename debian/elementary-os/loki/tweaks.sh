1c10a81f31bac5fd8231ae2b0153bba762a4cb14
puterakahfi <puterakahfi@gmail.com>
2017-03-02 10:13:04 +0700

    add tweaks

---
+++ debian/elementary-os/loki/tweaks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:philip.scott/elementary-tweaks
+
+sudo apt-get update
+sudo apt-get install -y  software-properties-common \
+            elementary-tweaks
+
+
+
+
