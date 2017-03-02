78e81e01cd70820e04205fd2b422d3a2c3520bdd
puterakahfi <puterakahfi@gmail.com>
2017-03-02 11:56:12 +0700

    add paper icon

---
+++ debian/themes/paper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+sudo add-apt-repository ppa:snwh/pulp
+# update repository info
+sudo apt-get update
+# install icon theme
+sudo apt-get install -y paper-icon-theme paper-gtk-theme paper-cursor-theme
