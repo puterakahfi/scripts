22c01c2bedde5cd424132dbc86ff6217cebe5e82
puterakahfi <puterakahfi@gmail.com>
2017-03-04 16:42:04 +0700

    add vim for terminal

---
+++ debian/terminal.sh
@@ -0,0 +1,13 @@
+#/usr/bin
+
+# install oh my zsh 
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#install tmux and tmux plugin
+sudo apt install tmux -y
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+# install vim 
+sudo apt install -y vim
