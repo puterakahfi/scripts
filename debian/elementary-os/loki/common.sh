908041a5b7d062ac23cbd56c282a8b4ab8e7b80d
puterakahfi <puterakahfi@gmail.com>
2017-03-05 06:52:59 +0700

    update common loki

--- debian/elementary-os/loki/common.sh
+++ debian/elementary-os/loki/common.sh
@@ -3,3 +3,16 @@
 #enable ppa 
 sudo apt-get install software-properties-common
 
+# install tlp to reduce overheating
+sudo apt install -y tlp tlp-rdw
+
+
+# install codec
+sudo apt install -y  libavcodec-extra ffmpeg
+
+#archive format
+sudo apt install -y rar unrar cabextract lzip lunzip arj unace p7zip-rar p7zip
+
+sudo apt install -y  ubuntu-restricted-extras
+
+
