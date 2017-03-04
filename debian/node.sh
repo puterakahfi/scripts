9509fcf87067e5a2aa58c487498810d1a12eb90d
puterakahfi <puterakahfi@gmail.com>
2017-03-04 19:36:15 +0700

    update node and yarn, add npm for node

--- debian/node.sh
+++ debian/node.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install python-software-properties -y
+sudo apt-get install python-software-properties 
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install nodejs -y
+sudo apt-get install nodejs npm  -y
+sudo ln -sf /usr/local/bin/nodejs /usr/local/bin/node
--- debian/yarn.sh
+++ debian/yarn.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+
+sudo apt-get update && sudo apt-get install -y yarn
+export PATH=$HOME/bin:/usr/local/bin:/usr/local/share/npm/bin:$PATH
+
 
