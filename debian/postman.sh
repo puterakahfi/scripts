ff7b8e19089b74b2d3fadcf8f8d1f1ddafd34545
puterakahfi <puterakahfi@gmail.com>
2017-03-10 11:27:54 +0700

    updated skype

--- debian/postman.sh
+++ debian/postman.sh
@@ -2,9 +2,9 @@
 
 
 wget    --output-document=postman.tar.gz https://dl.pstmn.io/download/latest/linux64
-tar -xvzf postman.tar.gz
-sudo mv -f  Postman/ /usr/share
+sudo tar -xvzf postman.tar.gz -C /usr/share/
 sudo ln -sf /usr/share/Postman/Postman /usr/local/bin/
 rm postman.tar.gz
+sudo cp Postman.desktop /usr/share/applications/
 
 
--- debian/skype.sh
+++ debian/skype.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-cd ~/Downloads/
+cd /tmp/
 if [ ! -f skypeforlinux-64-alpha.deb ]; then
     echo "file not founde, download from url..."
-    wget https://repo.skype.com/latest/skypeforlinux-64-alpha.deb
+    #wget https://repo.skype.com/latest/skypeforlinux-64-alpha.deb
+    wget --output-document=skype.deb https://go.skype.com/skypeforlinux-64-alpha.deb
 fi
 
 
 
 if hash gdebi 2>/dev/null; then
-    sudo gdebi skypeforlinux-64-alpha.deb
+    sudo gdebi skype.deb
 else
     echo "gdebi not found, please install gdebi first " 
 fi
